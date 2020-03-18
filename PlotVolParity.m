e5f317db5d42236339dba73134b9e0d77287b537
BenjaminSouane <40243046+blacksouane@users.noreply.github.com>
2020-03-18 14:46:06 +0100

    Add files via upload
    Code différent avec les codes pour les plots de la première stratégie

---
+++ PlotVolParity.m
@@ -0,0 +1,49 @@
+%% Plots Volatility Strategy
+
+%Plot of the Repartition of weights
+
+WeightsClasses = zeros(385,5);
+
+for i = 1:385
+    WeightsClasses(i,1) = sum(WeightsVolParity(i,1:7)) ;
+     WeightsClasses(i,2) = sum(WeightsVolParity(i,8:11)) ;
+      WeightsClasses(i,3) = sum(WeightsVolParity(i,12:21)) ;
+       WeightsClasses(i,4) = sum(WeightsVolParity(i,22:28)) ;
+        WeightsClasses(i,5) = sum(WeightsVolParity(i,29:35)) ;
+end
+
+%Plot of the Repartition of weights
+figure
+area(monthdate, WeightsClasses)
+colormap winter
+% Add a legend
+%legend('Energy', 'Fixed Income', 'Commodities', 'Equities', 'Currencies','Location','South','Orientation','horizontal')
+% Add title and axis labels
+title('Repartition of weights through asset classes')
+xlabel('Years')
+ylabel('Weights')
+ylim([0 1])
+
+%Plot of the Repartition of weights
+
+MCRClasses = zeros(385,5);
+
+for i = 1:385
+    MCRClasses(i,1) = sum(Margin(i,1:7)) ;
+     MCRClasses(i,2) = sum(Margin(i,8:11)) ;
+      MCRClasses(i,3) = sum(Margin(i,12:21)) ;
+       MCRClasses(i,4) = sum(Margin(i,22:28)) ;
+        MCRClasses(i,5) = sum(Margin(i,29:35)) ;
+end
+
+%Plot of the Repartition of weights
+figure
+area(monthdate(8:end), MCRClasses(8:end,:))
+colormap winter
+% Add a legend
+%legend('Energy', 'Fixed Income', 'Commodities', 'Equities', 'Currencies','Location','South','Orientation','horizontal')
+% Add title and axis labels
+title('Repartition of MCR through asset classes')
+xlabel('Years')
+ylabel('Weights')
+ylim([-10 110])
