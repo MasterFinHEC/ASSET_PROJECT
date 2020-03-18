a1ace955e563b84fa5bfbf175605b0605116e739
BenjaminSouane <40243046+blacksouane@users.noreply.github.com>
2020-03-18 14:44:41 +0100

    Update Main.m
    Changer la structure un peu et enlevé le code des plots

--- Main.m
+++ Main.m
@@ -2,7 +2,7 @@
 % EMPIRICAL METHODS FOR FINANCE
 % Homework I
 %
-% Benjamin Souane, Antoine-Michel Alexeev, Ludovic Suchet and ulien Bisch
+% Benjamin Souane, Antoine-Michel Alexeev, Ludovic Suchet and Julien Bisch
 % Due Date: 5 March 2020
 %==========================================================================
 
@@ -10,7 +10,7 @@ close all
 clc
 
 addpath(genpath('C:\Users\Benjamin\OneDrive\1. HEC\Master\MScF 4.2\5. QARM\2020\QARM_Project\VolParity'));
-
+addpath(genpath('C:\Users\Benjamin\OneDrive\1. HEC\Master\MScF 4.2\5. QARM\2020\QARM_Project\RiskParit'));
 %% Setup the Import Options and import the data
 opts = spreadsheetImportOptions("NumVariables", 36);
 
@@ -37,27 +37,28 @@ names = txt(2:end); %Vector of Names (Mainly used for plots)
 price = table2array(QARMDATA(2:end,2:end)); %Take out the date from the matrix of price
 date = datetime(table2array(QARMDATA(2:end,1))); %Vector of date
 
-%% Find the Returns
+%% Computing the the Returns and other usefull vectors
 
 [returns,firstData] = ReturnNaN(price); %So we have a vector of prices and NaN
 %firstdata : Vector with the index of each first data
 
-%% Weights of the volatility parity scheme 
+%Generating date 
+Date;
 
+%% Volatility Parity
+
+%Computing the Weights of the strategy
 WeightsVolParity = VolParity(returns,firstData); %Used a function to compute the weights
 
-%% Vector of signal
-
+%Computing the vector of signals
 Signal = SignalComputing(returns,firstData); %Computing all the signals 
-
-%% Portfolio volatility to compute leverage
 
 targetVol = 0.1; %Setting the target Volatility
 
+%Leverage and portfolio Volatility
 [leverage,PortfolioVol] = Leverage(WeightsVolParity,returns,targetVol); %Finding a vector of leverage, each period.
 
-%% Finding the returns of the strategy 
-
+% Finding the returns of the strategy 
 MonReturn = MonthlyReturns(returns); %Finding Monthly returns of the strategy
 Retour = ReturnStrategy(WeightsVolParity,leverage,MonReturn,Signal); %Finding the return of the entire strategy
 Date;
@@ -65,54 +66,33 @@ Date;
 %Cumulative Return
 cumReturn = cumprod(Retour+1); %Computing the cumulative return
 
-%Plot of the Repartition of weights
-
-WeightsClasses = zeros(385,5);
-
-for i = 1:385
-    WeightsClasses(i,1) = sum(WeightsVolParity(i,1:7)) ;
-     WeightsClasses(i,2) = sum(WeightsVolParity(i,8:11)) ;
-      WeightsClasses(i,3) = sum(WeightsVolParity(i,12:21)) ;
-       WeightsClasses(i,4) = sum(WeightsVolParity(i,22:28)) ;
-        WeightsClasses(i,5) = sum(WeightsVolParity(i,29:35)) ;
-end
-
-%Plot of the Repartition of weights
-figure
-area(monthdate, WeightsClasses)
-colormap winter
-% Add a legend
-%legend('Energy', 'Fixed Income', 'Commodities', 'Equities', 'Currencies','Location','South','Orientation','horizontal')
-% Add title and axis labels
-title('Repartition of weights through asset classes')
-xlabel('Years')
-ylabel('Weights')
-ylim([0 1])
-
-%% Find all the other ratios
-
+%Computing the marginal contribution to risk
 Margin = MCR(WeightsVolParity,returns,PortfolioVol);
 
-%Plot of the Repartition of weights
+PlotVolParity;
 
-MCRClasses = zeros(385,5);
+%% Risk parity
 
-for i = 1:385
-    MCRClasses(i,1) = sum(Margin(i,1:7)) ;
-     MCRClasses(i,2) = sum(Margin(i,8:11)) ;
-      MCRClasses(i,3) = sum(Margin(i,12:21)) ;
-       MCRClasses(i,4) = sum(Margin(i,22:28)) ;
-        MCRClasses(i,5) = sum(Margin(i,29:35)) ;
+%Finding the optimal weights through FminCon optimisation
+RiskPar = RiskParityOpti(Signal,WeightsVolParity,returns,targetVol);
+
+%Finding the grossweights in %
+GrossRiskPar = abs(RiskPar);
+for i = 1:length(GrossRiskPar)
+    total = sum(GrossRiskPar(i,:));
+    GrossRiskPar(i,:) = GrossRiskPar(i,:)*100/total;
 end
 
-%Plot of the Repartition of weights
-figure
-area(monthdate(8:end), MCRClasses(8:end,:))
-colormap winter
-% Add a legend
-%legend('Energy', 'Fixed Income', 'Commodities', 'Equities', 'Currencies','Location','South','Orientation','horizontal')
-% Add title and axis labels
-title('Repartition of MCR through asset classes')
-xlabel('Years')
-ylabel('Weights')
-ylim([-10 110])
+%Finding the leverage and the portfolio Volatility
+[LeverageRiskParity,PortfolioVolRiskPar] = Leverage(RiskPar,returns,targetVol); 
+
+%Computing the return of the strategy 
+ReturnRiskParity = ReturnStrategyRiskPar(RiskPar,LeverageRiskParity,MonReturn);
+
+%Cumulative Return
+CumuReturnRiskPar = cumprod(1+ReturnRiskParity);
+
+%Marginal Contribution to risk
+MarginRiskParity = MCR(RiskPar,returns,PortfolioVolRiskPar);
+
+PlotRiskParity;
